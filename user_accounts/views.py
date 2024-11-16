3a5b88e71dec5ad1469076bd0d4e33dae4038986
DogukanTaskiran <dogukantaskiran1222@gmail.com>
2024-11-16 22:14:11 +0300

    Register done

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm
 
 @login_required  
 def profile(request, user_id):
@@ -11,14 +12,15 @@ def profile(request, user_id):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile', user_id=user.id)
+            return redirect('user_accounts:profile', user_id=user.id)
     else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
 
 def accounts_index(request):
     return render(request, 'accounts_index.html')
@@ -28,7 +30,17 @@ def custom_logout(request, user_id):
     logout(request)
     return redirect('/')
 
-def custom_login(request, user_id):
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('user_accounts:profile', user_id=user.id)
+        else:
     
-    login(request)
-    return redirect('/')
+            return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'registration/login.html')
