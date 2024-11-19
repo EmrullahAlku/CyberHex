7fcae71b8ffb8a44847405ff7b760e9f50613e3d
DogukanTaskiran <dogukantaskiran1222@gmail.com>
2024-11-19 22:25:52 +0300

    homepage&layout html-css changes

--- notifications/views.py
+++ notifications/views.py
@@ -15,22 +15,19 @@ def create_comment_notification(user, post):
 def like_post(request, post_id):
     post = Post.objects.get(id=post_id)
     create_like_notification(post.user, post) 
-    # Continue with like logic...
+    
     return JsonResponse({'status': 'liked'})
 
 def comment_post(request, post_id):
     post = Post.objects.get(id=post_id)
-    create_comment_notification(post.user, post)  # Notify post owner
-    # Continue with comment logic...
+    create_comment_notification(post.user, post) 
+    
     return JsonResponse({'status': 'commented'})
 
-
 def notification_list(request):
-    # Retrieve unread notifications for the logged-in user
     notifications = Notification.objects.filter(user=request.user, read=False)
     
-    # If the user is viewing the page, mark notifications as read
     if request.method == "GET":
-        notifications.update(read=True)  # Mark all notifications as read when the user accesses them
+        notifications.update(read=True) 
 
     return render(request, 'notifications/notification_list.html', {'notifications': notifications})
